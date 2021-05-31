15808f524a62d5cb1bb058e892ed03fc69e63889
Aurélien <aurelien.delphin@gmail.com>
2021-05-31 11:40:22 +0200

    Added histograms in graph plot

--- plot/plotGraph.m
+++ plot/plotGraph.m
@@ -1,11 +1,21 @@
 function plotGraph(G, FOVx, FOVy, FOVz)
 
-figure;
 coords = cell2mat(G.Nodes.Coord);
 rfact = 10/max(G.Edges.r);
+
+figure;
+subplot(2,2,[1,3])
 plot(G, 'XData', coords(:,1), 'YData', coords(:,2),'ZData', coords(:,3), 'LineWidth',G.Edges.r*rfact)
 hold on
 xlim([0, FOVx])
 ylim([0, FOVy])
 zlim([0, FOVz])
 pbaspect([1,FOVy/FOVx,FOVz/FOVx])
+
+subplot(subplot(2,2,2))
+histogram(G.Edges.r, floor(numedges(G)/4))
+xlabel('Branches radii (m)')
+
+subplot(subplot(2,2,4))
+histogram(G.Edges.L, floor(numedges(G)/4))
+xlabel('Branches Lengths (m)')
